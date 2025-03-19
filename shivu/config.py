7011a864eec78f4c737564bf76fa21e3d6b8101c
surajdeveloper2 <surajget248@gmail.com>
2025-03-19 10:25:34 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -6,7 +6,7 @@ class Config(object):
     sudo_users = "6675050163", "6087651372","7640076990","1420481421","6645960688"
     GROUP_ID = -1002499806698
     TOKEN = "8175826981:AAGrz7p7NiE6t1vDIKDBTdbhHrcjB7MGDAo"
-    mongo_url = "mongodb+srv://naruto:hinatababy@cluster0.rqyiyzx.mongodb.net/"
+    mongo_url = "mongodb+srv://sunnymuduli584:<db_password>@sunny.s9g6h.mongodb.net/"
     PHOTO_URL = ["https://graph.org/file/09e83a1d89aceabd480c5-2afc46a31083fe23f2.jpg", "https://graph.org/file/0aa659508c1add9ae4c86-2b335aa5262b7b64d2.jpg"]
     SUPPORT_CHAT = "Anime_Circle_Club"
     UPDATE_CHAT = "Waifu_Chan_Bot_updates"
