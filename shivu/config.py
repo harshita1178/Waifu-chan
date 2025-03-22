0541c746de4cdd572a0dda1f687dc1365225f5f2
harshita1178 <usekarlomujhe@gmail.com>
2025-03-22 18:40:16 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -1,13 +1,13 @@
 class Config(object):
     LOGGER = True
-    # Get this value from my.telegram.org/apps
-    OWNER_ID = 6675050163  # Updated Owner ID
-    sudo_users = [7640076990, 7756901810, 7885908019]  # Only 3 sudo users
-    GROUP_ID = -1002499806698  # Ensure it's an integer
+    OWNER_ID = 6675050163
+    sudo_users = [7640076990, 7756901810, 7885908019]
+    GROUP_ID = -1002499806698
     TOKEN = "7583740884:AAHNM9iVB-bWa3T4USseHrTavjBV8P6lAag"
-    mongo_url = "mongodb+srv://naruto:hinatababy@cluster0.rqyiyzx.mongodb.net"
     
-    # Ensure full URLs are used in the list
+    # Updated MongoDB URI
+    mongo_url = "mongodb+srv://Harsh0987:ukdxckfSzJqlMwqQ@cluster0.mlw...mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
     PHOTO_URL = [
         "https://graph.org/file/09e83a1d89aceabd480c5-2afc46a31083fe23f2.jpg"
     ]
@@ -15,8 +15,8 @@ class Config(object):
     SUPPORT_CHAT = "Anime_Circle_Club"
     UPDATE_CHAT = "Waifu_Chan_Bot_updates"
     BOT_USERNAME = "@Waifu_Chan_Robot"
-    CHARA_CHANNEL_ID = -1002640379822  # Ensure it's an integer
-    api_id = 28480539  # Ensure it's an integer
+    CHARA_CHANNEL_ID = -1002640379822
+    api_id = 28480539
     api_hash = "6320d9f1bc1f0b72ad66ebdb9d6bfc2c"
 
 class Production(Config):
