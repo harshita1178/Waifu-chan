bdece6f2ce1157e5e31951dd41306ad5bc544cf8
harshita1178 <usekarlomujhe@gmail.com>
2025-04-01 23:13:55 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -1,23 +1,23 @@
 class Config(object):
     LOGGER = True
-    OWNER_ID = 6675050163
+    OWNER_ID = 8156600797
     sudo_users = [7640076990, 7756901810, 7885908019]
     GROUP_ID = -1002499806698
-    TOKEN = "7583740884:AAHNM9iVB-bWa3T4USseHrTavjBV8P6lAag"
+    TOKEN = "7699976588:AAGltTt0NN-lS6m5daTfdIWUBjbX7OKZ1Wc"
     
     # Updated MongoDB URI
-    mongo_url = "mongodb+srv://naruto:hinatababy@cluster0.rqyiyzx.mongodb.net/"
+    mongo_url = "mongodb+srv://dekhobeta82:TGDARK11798@minato22.sfngv7t.mongodb.net/?retryWrites=true&w=majority&appName=minato22"
 
     PHOTO_URL = [
         "https://graph.org/file/09e83a1d89aceabd480c5-2afc46a31083fe23f2.jpg"
     ]
     
     SUPPORT_CHAT = "Anime_Circle_Club"
-    UPDATE_CHAT = "Waifu_Chan_Bot_updates"
-    BOT_USERNAME = "@Waifu_Chan_Robot"
+    UPDATE_CHAT = "@Madara_X_Ultra_Waifus_Updates"
+    BOT_USERNAME = "@Madara_X_Ultra_WaifusBot"
     CHARA_CHANNEL_ID = -1002640379822
-    api_id = 28480539
-    api_hash = "6320d9f1bc1f0b72ad66ebdb9d6bfc2c"
+    api_id = 28159105
+    api_hash = "a0936ddf210a7e091e19947c7dc70c91"
 
 class Production(Config):
     LOGGER = True
